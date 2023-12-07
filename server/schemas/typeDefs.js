6d7855bfbdded73f563431bbf1eb33a991884330
DistantDig <jameswaller711@gmail.com>
2023-12-06 16:46:21 -0800

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const typeDefs = `
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]!
+    }
+
+    type Query {
+        user(userId: ID!): User!
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): User
+        saveBook(userId: ID!, bookId: ID!): User
+        deleteBook(userId: ID!, bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
